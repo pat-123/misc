27f636d67db0a16f2b6ac2a65c8930298cf6c868
pat@123 <thapliyalpooja69@gmail.com>
2019-08-18 16:43:26 +0530

    base commit

---
+++ misc/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 13 13:38:47 2019
+
+@author: PAT
+"""
+
+
+import os
+import sys
+from sklearn.datasets import load_iris
+import math
+import pandas as pd
+import seaborn  as sb
+import matplotlib.pyplot as plt
+
+from pylab import rcParams
+rcParams['figure.figsize']=7,5
+plt.style.use('seaborn-whitegrid')
+
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from scipy.stats import chi2_contingency
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+
+from sklearn.linear_model import LinearRegression
+# add to sys paths
+#sys.path.append('C:\\Users\\PAT\\Documents\\edwisor\\projects\\bigmart_sales')
+def rt_parent():
+    parent_dir = 'TBD---------'
+    return parent_dir
---
+++ misc/config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 13 13:34:22 2019
+
+@author: PAT
+"""
+
+from pathlib import Path
+
+data_dir = Path('C:/Users/PAT/Documents/edwisor/projects/misc/data/raw')
+#test_data_path = data_dir  / 'test.csv'
+train_data_path = data_dir  / 'regr.csv'
+fs_data_path = data_dir  / 'fs_train.csv'
