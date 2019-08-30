160955e1c5c8513bd0440573b441e13e61dc0681
pat@123 <thapliyalpooja69@gmail.com>
2019-08-30 16:50:49 +0530

    images in notebook

--- misc/config.py
+++ misc/config.py
@@ -8,6 +8,12 @@ Created on Tue Aug 13 13:34:22 2019
 from pathlib import Path
 
 data_dir = Path('C:/Users/PAT/Documents/edwisor/projects/misc/data/raw')
+proc_data_dir = Path('C:/Users/PAT/Documents/edwisor/projects/misc/data/processed')
 #test_data_path = data_dir  / 'test.csv'
 train_data_path = data_dir  / 'regr.csv'
 fs_data_path = data_dir  / 'fs_train.csv'
+img_1 = '..\\data\\processed\\1.png'
+img_2 = '..\\data\\processed\\2.png'
+img_3 = '..\\data\\processed\\3.png'
+img_4 = '..\\data\\processed\\4.png'
+
