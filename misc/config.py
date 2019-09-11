44ab7d047980c4626de0bbf3765b8fc536e4d2de
pat@123 <thapliyalpooja69@gmail.com>
2019-09-11 18:16:02 +0530

    cleanup

--- misc/config.py
+++ misc/config.py
@@ -12,8 +12,10 @@ proc_data_dir = Path('C:/Users/PAT/Documents/edwisor/projects/misc/data/processe
 #test_data_path = data_dir  / 'test.csv'
 train_data_path = data_dir  / 'regr.csv'
 fs_data_path = data_dir  / 'fs_train.csv'
+inp_data_path = data_dir  / 'inp_data.csv'
 img_1 = '..\\data\\processed\\1.png'
 img_2 = '..\\data\\processed\\2.png'
 img_3 = '..\\data\\processed\\3.png'
 img_4 = '..\\data\\processed\\4.png'
+nb_file_path = data_dir  / 'nb_data.csv'
 
